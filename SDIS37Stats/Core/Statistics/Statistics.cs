b02a940b21a172208f780dafd27629d68ab19484
syukoGit <antonin2K@orange.fr>
2021-01-24 20:22:29 +0100

    Statistics for : - number operations today - last updated

--- SDIS37Stats/Core/Statistics/Statistics.cs
+++ SDIS37Stats/Core/Statistics/Statistics.cs
@@ -1,18 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Web;
 using System.Windows.Forms;
 
 namespace SDIS37Stats.Core.Statistics
 {
-    static class Statistics
+    class Statistics
     {
+        private readonly Web.WebService webService;
+
+        private readonly System.Globalization.CultureInfo dateTimeProvider = new System.Globalization.CultureInfo("fr-FR");
+        private const string DateTimeFormat = "g";
+
+        public delegate void OnStatUpdatedHandler();
+        public OnStatUpdatedHandler OnStatUpdated;
+
+        public Statistics(Web.WebService webService)
+        {
+            this.webService = webService;
+            this.webService.OnHtmlDocStats += this.WebService_OnHtmlDocStats;
+        }
+
+        public int TotalOperationInDay { get; private set; }
+
+        public DateTime LastRefresh { get; private set; }
+
+        public int[] OperationPerHour { get; private set; } = new int[23];
         
+        #region Event
+        private void WebService_OnHtmlDocStats(HtmlDocument htmlDocument)
+        {
+            string dateTimeStr = htmlDocument.GetElementById("date").InnerText + " " + htmlDocument.GetElementById("last_refresh").InnerText;
+
+            this.TotalOperationInDay = int.Parse(htmlDocument.GetElementById("number-interventions").InnerText);
+            this.LastRefresh = DateTime.ParseExact(dateTimeStr, DateTimeFormat, this.dateTimeProvider);
+
+            this.OnStatUpdated?.Invoke();
+        }
+        #endregion
     }
 }
