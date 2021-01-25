11580abb5b4bf758f841d0ca909c665a173dbc06
syukoGit <antonin2K@orange.fr>
2021-01-25 14:13:09 +0100

    Color class

---
+++ SDIS37Stats/Controls/Theme/Color/Theme.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+namespace SDIS37Stats.Controls.Theme
+{
+    public abstract class MyColor
+    {
+        protected static readonly Color dark1 = Color.FromArgb(64, 64, 64); //Background
+        protected static readonly Color dark2 = Color.FromArgb(75, 75, 75);
+
+        protected static readonly Color white1 = Color.FromArgb(240, 240, 240); // FontColor
+
+        protected static readonly Color yellow1 = Color.FromArgb(220, 220, 0);
+
+        protected static readonly Color debugColor = Color.Green;
+    }
+
+    public abstract class Default : MyColor
+    {
+        public static Color BackgroundColor => dark1;
+        public static Color FontColor => white1;
+        public static Color DebugColor => debugColor;
+    }
+
+    public abstract class Graph : MyColor
+    {
+        public static Color BackgroundColor => dark2;
+        public static Color AxisColor => yellow1;
+        public static Color MainBarColor => white1;
+    }
+}
