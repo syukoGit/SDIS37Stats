8e08a96c3d6763e8462934d35f4e5cb1ef19f418
syukoGit <antonin2K@orange.fr>
2021-02-11 22:25:10 +0100

    Prevent Windows from going to sleep

--- SDIS37Stats/Program.cs
+++ SDIS37Stats/Program.cs
@@ -3,45 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace SDIS37Stats
 {
     static class Program
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern uint SetThreadExecutionState([In] uint esFlags);
+
+
         /// <summary>
         /// Point d'entrée principal de l'application.
         /// </summary>
         [STAThread]
         static void Main(string[] args)
         {
-            if (args.Length > 0)
+            try
             {
-                for (int i = 0; i < args.Length; i++)
+                SetThreadExecutionState(0x80000000 | 0x00000002 | 0x00000040);
+
+                if(args.Length > 0)
                 {
-                    if (args[i] == "-u")
+                    SetUsernamePassword(args);
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Controls.MainForm());
+            }
+            finally
+            {
+                SetThreadExecutionState(0x80000000);
+                Console.Out.WriteLine("Arrêt du programme");
+            }
+        }
+
+        static void SetUsernamePassword(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "-u")
+                {
+                    i++;
+                    if (i < args.Length)
                     {
-                        i++;
-                        if (i < args.Length)
-                        {
-                            Core.Web.WebService.Username = args[i];
-                            Console.Out.WriteLine("Username : " + args[i]);
-                        }
+                        Core.Web.WebService.Username = args[i];
+                        Console.Out.WriteLine("Username : " + args[i]);
                     }
-                    else if (args[i] == "-p")
+                }
+                else if (args[i] == "-p")
+                {
+                    i++;
+                    if (i < args.Length)
                     {
-                        i++;
-                        if (i < args.Length)
-                        {
-                            Core.Web.WebService.Password = args[i];
-                            Console.Out.WriteLine("Password : " + args[i]);
-                        }
+                        Core.Web.WebService.Password = args[i];
+                        Console.Out.WriteLine("Password : " + args[i]);
                     }
                 }
             }
-
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Controls.MainForm());
         }
     }
 }
