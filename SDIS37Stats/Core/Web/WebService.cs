2f978939fe8ba73d57aec4aaf6c763ec75e09c06
syukoGit <antonin2K@orange.fr>
2021-02-15 16:09:25 +0100

    Added new file for regroup all WebService urls

--- SDIS37Stats/Core/Web/WebService.cs
+++ SDIS37Stats/Core/Web/WebService.cs
@@ -9,38 +9,8 @@
     /// <remarks>
     /// It uses a <see cref="System.Windows.Forms.WebBrowser"/> so objects can disposable.
     /// </remarks>
-    class WebService : IDisposable
+    class WebService : WebServiceURL, IDisposable
     {
-        /// <summary>
-        /// Const string used for get the login url of the webservice.
-        /// </summary>
-        public const string WebServicesLoginURL = "https://webservices.sdis37.fr/users/login";
-
-        /// <summary>
-        /// Const string used for get the main page of the webservice.
-        /// </summary>
-        public const string WebServiceMainPageURL = "https://webservices.sdis37.fr/interventions";
-
-        /// <summary>
-        /// Const string used for get the number of operation in day.
-        /// </summary>
-        public const string WebServiceNbOperationsURl = "https://webservices.sdis37.fr/interventions/nbInterventions";
-
-        /// <summary>
-        /// Const string used for get the number of operations per hour.
-        /// </summary>
-        public const string WebServiceStatsForOperationPerHourURL = "https://webservices.sdis37.fr/interventions/getNb";
-
-        /// <summary>
-        /// Const string used for get the lis of recent operations in SDIS37.
-        /// </summary>
-        public const string WebServiceRecentOperationListURL = "https://webservices.sdis37.fr/interventions/liste?direction=desc&sort=Depart";
-
-        /// <summary>
-        /// Const string used for get the availabilities of the firefighters of the user's firehouse.
-        /// </summary>
-        public const string WebServiceFirefighterAvailabilityURL = "https://webservices.sdis37.fr/personnels";
-
         /// <summary>
         /// Public string for save the username used for connection.
         /// </summary>
---
+++ SDIS37Stats/Core/Web/WebServiceURL.cs
@@ -0,0 +1,35 @@
+﻿namespace SDIS37Stats.Core.Web
+{
+    internal class WebServiceURL
+    {
+        /// <summary>
+        /// Const string used for get the login url of the webservice.
+        /// </summary>
+        public const string WebServicesLoginURL = "https://webservices.sdis37.fr/users/login";
+
+        /// <summary>
+        /// Const string used for get the main page of the webservice.
+        /// </summary>
+        public const string WebServiceMainPageURL = "https://webservices.sdis37.fr/interventions";
+
+        /// <summary>
+        /// Const string used for get the number of operation in day.
+        /// </summary>
+        public const string WebServiceNbOperationsURl = "https://webservices.sdis37.fr/interventions/nbInterventions";
+
+        /// <summary>
+        /// Const string used for get the number of operations per hour.
+        /// </summary>
+        public const string WebServiceStatsForOperationPerHourURL = "https://webservices.sdis37.fr/interventions/getNb";
+
+        /// <summary>
+        /// Const string used for get the lis of recent operations in SDIS37.
+        /// </summary>
+        public const string WebServiceRecentOperationListURL = "https://webservices.sdis37.fr/interventions/liste?direction=desc&sort=Depart";
+
+        /// <summary>
+        /// Const string used for get the availabilities of the firefighters of the user's firehouse.
+        /// </summary>
+        public const string WebServiceFirefighterAvailabilityURL = "https://webservices.sdis37.fr/personnels";
+    }
+}
