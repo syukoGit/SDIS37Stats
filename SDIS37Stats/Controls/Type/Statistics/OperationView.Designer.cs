f7b579fd1b151804b76ac0637cf66443a371740e
syukoGit <antonin2K@orange.fr>
2021-03-31 12:39:34 +0200

    Adding and changing files for using .NET 5.0

--- SDIS37Stats/Controls/Type/Statistics/OperationView.Designer.cs
+++ SDIS37Stats/Controls/Type/Statistics/OperationView.Designer.cs
@@ -59,19 +59,21 @@
             this.tableLayoutPanel1.Controls.Add(this.pictureBoxOperationType, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(507, 64);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(513, 74);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // time
             // 
             this.time.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.time.AutoSize = true;
-            this.time.Location = new System.Drawing.Point(4, 25);
+            this.time.Location = new System.Drawing.Point(5, 29);
+            this.time.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.time.Name = "time";
-            this.time.Size = new System.Drawing.Size(95, 13);
+            this.time.Size = new System.Drawing.Size(95, 15);
             this.time.TabIndex = 0;
             this.time.Text = "25/01/2021 05:47";
             this.time.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -80,9 +82,10 @@
             // 
             this.numOperation.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.numOperation.AutoSize = true;
-            this.numOperation.Location = new System.Drawing.Point(128, 25);
+            this.numOperation.Location = new System.Drawing.Point(133, 29);
+            this.numOperation.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.numOperation.Name = "numOperation";
-            this.numOperation.Size = new System.Drawing.Size(55, 13);
+            this.numOperation.Size = new System.Drawing.Size(55, 15);
             this.numOperation.TabIndex = 1;
             this.numOperation.Text = "21002087";
             // 
@@ -94,21 +97,22 @@
             this.tableLayoutPanel2.Controls.Add(this.localisation, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.OperationDescription, 0, 1);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(190, 4);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(197, 4);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 2;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(248, 56);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(246, 66);
             this.tableLayoutPanel2.TabIndex = 2;
             // 
             // localisation
             // 
             this.localisation.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.localisation.AutoSize = true;
-            this.localisation.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.localisation.Location = new System.Drawing.Point(3, 10);
-            this.localisation.Margin = new System.Windows.Forms.Padding(3, 10, 3, 5);
+            this.localisation.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.localisation.Location = new System.Drawing.Point(4, 14);
+            this.localisation.Margin = new System.Windows.Forms.Padding(4, 12, 4, 6);
             this.localisation.Name = "localisation";
             this.localisation.Size = new System.Drawing.Size(109, 13);
             this.localisation.TabIndex = 0;
@@ -119,10 +123,10 @@
             // 
             this.OperationDescription.AutoSize = true;
             this.OperationDescription.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.OperationDescription.Location = new System.Drawing.Point(3, 33);
-            this.OperationDescription.Margin = new System.Windows.Forms.Padding(3, 5, 3, 10);
+            this.OperationDescription.Location = new System.Drawing.Point(4, 39);
+            this.OperationDescription.Margin = new System.Windows.Forms.Padding(4, 6, 4, 12);
             this.OperationDescription.Name = "OperationDescription";
-            this.OperationDescription.Size = new System.Drawing.Size(242, 13);
+            this.OperationDescription.Size = new System.Drawing.Size(238, 15);
             this.OperationDescription.TabIndex = 1;
             this.OperationDescription.Text = "DOMICILE : PERSONNE RESTANT A TERRE";
             // 
@@ -131,9 +135,10 @@
             this.vehicules.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.vehicules.AutoSize = true;
             this.vehicules.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.vehicules.Location = new System.Drawing.Point(445, 25);
+            this.vehicules.Location = new System.Drawing.Point(452, 29);
+            this.vehicules.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.vehicules.Name = "vehicules";
-            this.vehicules.Size = new System.Drawing.Size(57, 13);
+            this.vehicules.Size = new System.Drawing.Size(55, 15);
             this.vehicules.TabIndex = 3;
             this.vehicules.Text = "VSAV RID";
             this.vehicules.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -141,23 +146,24 @@
             // pictureBoxOperationType
             // 
             this.pictureBoxOperationType.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBoxOperationType.Location = new System.Drawing.Point(106, 24);
+            this.pictureBoxOperationType.Location = new System.Drawing.Point(109, 29);
+            this.pictureBoxOperationType.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.pictureBoxOperationType.Name = "pictureBoxOperationType";
             this.pictureBoxOperationType.Size = new System.Drawing.Size(15, 15);
             this.pictureBoxOperationType.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBoxOperationType.TabIndex = 4;
             this.pictureBoxOperationType.TabStop = false;
             // 
-            // DisplayOperation
+            // OperationView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 1, 3, 0);
-            this.Name = "DisplayOperation";
-            this.Size = new System.Drawing.Size(507, 64);
+            this.Margin = new System.Windows.Forms.Padding(4, 1, 4, 0);
+            this.Name = "OperationView";
+            this.Size = new System.Drawing.Size(513, 74);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
